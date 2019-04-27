a59041ed2a73c9832acbcdc787a5b8b8d85337da
Mpale <1120143451@qq.com>
2019-04-28 01:45:20 +0800

    vue 初始化

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  div#app
+    img(src="./assets/logo.png")
+    h1 Welcome from pug & sass
+    HelloWorld
+  <!-- <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+  </div> -->
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+
+  h1 {
+    color: #42b983;
+  }
+}
+</style>
